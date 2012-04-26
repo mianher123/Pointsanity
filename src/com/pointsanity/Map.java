94d735216da49d5a0950bd6248d54d9cd1fb273e
mianher <jackychen790823@hotmail.com>
2012-04-27 00:25:25 +0800

    distance/map finished

---
+++ src/com/pointsanity/Map.java
@@ -0,0 +1,324 @@
+package com.pointsanity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Toast;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class Map extends MapActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.mapview);
+//        MapView map = new MapView(this, "0vUBBiZoYQvqxxLi0pgax3xP_ccG6zB-WhI4U5Q");
+//        setContentView(map);
+        findViews();
+        setupMap();
+    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	private MapView map;
+    private MapController controller;
+
+    private void findViews() {
+            map = (MapView) findViewById(R.id.map);
+            controller = map.getController();
+    }
+
+    private MyLocationOverlay locLayer;
+    private LandMarkOverlay markLayer;
+    private void setupMap() {
+            GeoPoint ntu = new GeoPoint(
+                            (int) (25.018476 * 1000000),
+                            (int) (121.538615 * 1000000)
+            );
+            map.setTraffic(false);
+            map.setBuiltInZoomControls(true);
+            controller.setZoom(17);
+            controller.animateTo(ntu);
+    		
+//            List<Overlay> overlays = map.getOverlays();
+    		locLayer = new MyLocationOverlay(this, map);
+    		map.getOverlays().add(locLayer);
+    		locLayer.runOnFirstFix(new Runnable() {
+                public void run() {
+                    // Zoom in to current location
+                    map.setTraffic(true);
+                    controller.setZoom(15);
+                    controller.animateTo(locLayer.getMyLocation());
+                }
+            });
+//            overlays.add(locLayer);
+    		
+    		Drawable pin=getResources().getDrawable(android.R.drawable.btn_star_big_on);
+            pin.setBounds(0, 0, pin.getMinimumWidth(), pin.getMinimumHeight());
+
+            markLayer = new LandMarkOverlay(pin);
+            map.getOverlays().add(markLayer);
+    }
+
+	protected static final int MENU_HAND = Menu.FIRST;
+    protected static final int MENU_LAND = Menu.FIRST+1;
+    protected static final int MENU_APPLE = Menu.FIRST+2;
+    protected static final int MENU_COFFEE = Menu.FIRST+3;
+    protected static final int MENU_FAMILY = Menu.FIRST+4;
+    protected static final int MENU_FIRE = Menu.FIRST+5;
+    protected static final int MENU_KAKA = Menu.FIRST+6;
+    protected static final int MENU_KO = Menu.FIRST+7;
+    protected static final int MENU_ONE = Menu.FIRST+8;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+            // TODO Auto-generated method stub
+    	menu.add(0, MENU_HAND, 0, "歇手停");
+        menu.add(0, MENU_LAND, 0, "69嵐");
+        menu.add(0, MENU_APPLE, 0, "小蘋果");
+        menu.add(0, MENU_COFFEE, 0, "老咖啡");
+        menu.add(0, MENU_FAMILY, 0, "親家便利商店");
+        menu.add(0, MENU_FIRE, 0, "阿火茶鋪");
+        menu.add(0, MENU_KAKA, 0, "KaKa真假");
+        menu.add(0, MENU_KO, 0, "KO便利商店");
+        menu.add(0, MENU_ONE, 0, "ONE便利商店");
+            return super.onCreateOptionsMenu(menu);
+    }
+
+    GeoPoint hand = new GeoPoint(
+                (int) (25.018009 * 1000000),
+                    (int) (121.540192 * 1000000)
+            );
+    GeoPoint land = new GeoPoint(
+                (int) (25.014461 * 1000000),
+                    (int) (121.533465 * 1000000)
+            );
+    GeoPoint apple = new GeoPoint(
+                (int) (25.016055 * 1000000),
+                    (int) (121.532414 * 1000000)
+            );
+    GeoPoint coffee = new GeoPoint(
+            (int) (25.015773 * 1000000),
+                (int) (121.53339 * 1000000)
+        );
+    GeoPoint family = new GeoPoint(
+            (int) (25.021976 * 1000000),
+                (int) (121.541179 * 1000000)
+        );
+    GeoPoint fire = new GeoPoint(
+            (int) (25.015151 * 1000000),
+                (int) (121.536362 * 1000000)
+        );
+    GeoPoint kaka = new GeoPoint(
+            (int) (25.014723 * 1000000),
+                (int) (121.534367 * 1000000)
+        );
+    GeoPoint ko = new GeoPoint(
+            (int) (25.017076 * 1000000),
+                (int) (121.530848 * 1000000)
+        );
+    GeoPoint one = new GeoPoint(
+            (int) (25.017173 * 1000000),
+                (int) (121.544849 * 1000000)
+        );
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+            // TODO Auto-generated method stub
+            super.onOptionsItemSelected(item);
+            switch(item.getItemId()) {
+                    case MENU_HAND:
+                            controller.animateTo(hand);
+                            break;
+                    case MENU_LAND:
+                            controller.animateTo(land);
+                            break;
+                    case MENU_APPLE:
+                            controller.animateTo(apple);
+                            break;
+                    case MENU_COFFEE:
+                        controller.animateTo(coffee);
+                        break;
+                    case MENU_FAMILY:
+                        controller.animateTo(family);
+                        break;
+                    case MENU_FIRE:
+                        controller.animateTo(fire);
+                        break;
+                    case MENU_KAKA:
+                    	controller.animateTo(kaka);
+                    	break;
+                    case MENU_KO:
+                    	controller.animateTo(ko);
+                    	break;
+                    case MENU_ONE:
+                    	controller.animateTo(one);
+                    	break;
+            }
+            return super.onOptionsItemSelected(item);
+    }
+    
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_I || keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            // Zooming In
+//            controller.setZoom(map.getZoomLevel() + 1);
+        	controller.setZoom(Math.min(map.getMaxZoomLevel(), map.getZoomLevel() + 1));
+        	return true;
+        } else if (keyCode == KeyEvent.KEYCODE_O || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            // Zooming Out
+//            controller.setZoom(map.getZoomLevel() - 1);
+        	controller.setZoom(Math.max(15, map.getZoomLevel() - 1));
+        	return true;
+        } else if (keyCode == KeyEvent.KEYCODE_S) {
+            // Switch to satellite view
+            map.setSatellite(true) ;
+            map.setTraffic(false);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_T) {
+            // Switch on traffic overlays
+            map.setSatellite(false) ;
+            map.setTraffic(true);
+            return true;
+        }
+        return false;
+    }
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		
+		//check connectivity
+    	ConnectivityManager connectivityMgr =  (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+    	NetworkInfo netInfo = connectivityMgr.getActiveNetworkInfo();
+    	if (netInfo == null) {
+    		new AlertDialog.Builder(Map.this)
+        	.setTitle("No Connection")
+        	.setMessage("Please enable internet connection before use this app")
+        	.setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+    			public void onClick(DialogInterface dialog, int whichButton) {
+    		    	startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+    			}
+        	})
+        	.show();
+    	} else {
+    		locLayer.enableMyLocation();
+    	}
+	}
+	
+    @Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		locLayer.disableMyLocation();
+	}
+    
+    private class LandMarkOverlay extends ItemizedOverlay<OverlayItem> {
+
+        private List<OverlayItem> items = new ArrayList<OverlayItem>();
+
+        public LandMarkOverlay(Drawable defaultMarker) {
+                super(defaultMarker);
+                // TODO Auto-generated constructor stub
+                items.add(
+                    new OverlayItem(
+                                hand,
+                                "台北",
+                                "歇手停")
+                        );
+                items.add(
+                    new OverlayItem(
+                                land,
+                                "台中",
+                                "69嵐")
+                        );
+                items.add(
+                    new OverlayItem(
+                                apple,
+                                "高雄",
+                                "小蘋果")
+                        );
+                items.add(
+                        new OverlayItem(
+                                    coffee,
+                                    "高雄",
+                                    "老咖啡")
+                            );
+                items.add(
+                        new OverlayItem(
+                                    family,
+                                    "高雄",
+                                    "親家便利商店")
+                            );
+                items.add(
+                        new OverlayItem(
+                                    fire,
+                                    "高雄",
+                                    "阿火茶舖")
+                            );
+                items.add(
+                        new OverlayItem(
+                                    kaka,
+                                    "高雄",
+                                    "KaKa真假")
+                            );
+                items.add(
+                        new OverlayItem(
+                                    ko,
+                                    "高雄",
+                                    "KO便利商店")
+                            );
+                items.add(
+                        new OverlayItem(
+                                    one,
+                                    "高雄",
+                                    "ONE便利商店")
+                            );
+                populate();
+        }
+
+        @Override
+        protected OverlayItem createItem(int i) {
+                // TODO Auto-generated method stub
+                return items.get(i);
+        }
+
+        @Override
+        public int size() {
+                // TODO Auto-generated method stub
+                return items.size();
+        }
+
+    @Override
+    protected boolean onTap(int pIndex) {
+        Toast.makeText(Map.this,
+            "" + items.get(pIndex).getSnippet(),
+            Toast.LENGTH_SHORT).show();
+        return true;
+    }
+}
+}
--- src/com/pointsanity/ShopList.java
+++ src/com/pointsanity/ShopList.java
@@ -17,12 +17,17 @@
 package com.pointsanity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
@@ -49,22 +54,32 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.nio.charset.Charset;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 
 
+
 public class ShopList extends Activity implements CreateNdefMessageCallback,
-        OnNdefPushCompleteCallback {
+        OnNdefPushCompleteCallback, LocationListener{
     NfcAdapter mNfcAdapter;
     TextView mInfoText;
     ImageView mNFC;
     ImageView mWifi;
+    ImageView mRefresh;
+    ImageView mMap;
     private ListView mListView;
     private static final int MESSAGE_SENT = 1;
     String FBID;
-    ArrayList<HashMap<String,Object>> mList = new ArrayList<HashMap<String,Object>>();
+    ArrayList<HashMap<String,Object>> mList = new ArrayList<HashMap<String,Object>>();   
+    private LocationManager mgr;
+    private String best;
+    private double shopPose[][] = {{25.018009,121.540192}/*活大歇腳亭*/,{25.014461,121.533465}/*汀洲路50嵐*/,{25.016055,121.532414}/*橘菓子*/,{25.015773,121.53339}/*Starbucks*/,{25.021976,121.541179}/*後門全家*/,{25.015151,121.536362}/*小小福阿水*/,{25.014723,121.534367}/*公館Coco*/,{25.017076,121.530848}/*三總旁OK*/,{25.017173,121.544849}/*長興小七*/};
+    Location location;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,6 +91,8 @@ public class ShopList extends Activity implements CreateNdefMessageCallback,
         mListView = (ListView) findViewById(R.id.listView1);
        	mNFC = (ImageView) findViewById(R.id.nfcView1);
         mWifi = (ImageView) findViewById(R.id.wifiView1);
+        mRefresh = (ImageView) findViewById(R.id.refreshView1);
+        mMap = (ImageView) findViewById(R.id.mapView1);
         if(checkWifiStatus()){
         	mWifi.setImageResource(R.drawable.wifi_on);
         	
@@ -83,67 +100,9 @@ public class ShopList extends Activity implements CreateNdefMessageCallback,
         else
         	mWifi.setImageResource(R.drawable.wifi_off);
        	
-       	HashMap<String,Object> item = new HashMap<String,Object>();
-        item.put("icon", R.drawable.hand);
-        item.put("shopname", "店名:歇手停");
-        item.put("distance", "距離:3公里");
-        mList.add(item);
+        //updateDistanceInfo();
         
-        item = new HashMap<String,Object>();
-        item.put("icon", R.drawable.land);
-        item.put("shopname", "店名:69嵐");
-        item.put("distance", "距離:5公里");
-        mList.add(item);
-        
-        item = new HashMap<String,Object>();
-        item.put("icon", R.drawable.apple);
-        item.put("shopname", "店名:小蘋果");
-        item.put("distance", "距離:5公里");
-        mList.add(item);
-        
-        item = new HashMap<String,Object>();
-        item.put("icon", R.drawable.coffee);
-        item.put("shopname", "店名:老咖啡");
-        item.put("distance", "距離:5公里");
-        mList.add(item);
-        
-        item = new HashMap<String,Object>();
-        item.put("icon", R.drawable.family);
-        item.put("shopname", "店名:親家便利商店");
-        item.put("distance", "距離:5公里");
-        mList.add(item);
-        
-        item = new HashMap<String,Object>();
-        item.put("icon", R.drawable.fire);
-        item.put("shopname", "店名:阿火茶鋪");
-        item.put("distance", "距離:5公里");
-        mList.add(item);
-        
-        item = new HashMap<String,Object>();
-        item.put("icon", R.drawable.kaka);
-        item.put("shopname", "店名:KaKa真假");
-        item.put("distance", "距離:5公里");
-        mList.add(item);
-        
-        item = new HashMap<String,Object>();
-        item.put("icon", R.drawable.ko);
-        item.put("shopname", "店名:KO便利商店");
-        item.put("distance", "距離:5公里");
-        mList.add(item);
-        
-        item = new HashMap<String,Object>();
-        item.put("icon", R.drawable.one);
-        item.put("shopname", "ONE便利商店");
-        item.put("distance", "距離:5公里");
-        mList.add(item);
-        
-       	SimpleAdapter sAdapter;
-        sAdapter = new SimpleAdapter(this, mList, R.layout.listitem, 
-        		   new String[] {"icon","shopname","distance"}, 
-        		   new int[] {R.id.ItemImage,R.id.ItemTitle, R.id.ItemDist}  );
-
-        mListView.setAdapter(sAdapter);
-        mListView.setTextFilterEnabled(true);
+       	
        	
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (mNfcAdapter == null || !mNfcAdapter.isEnabled()) {
@@ -160,6 +119,21 @@ public class ShopList extends Activity implements CreateNdefMessageCallback,
         		
         	};
        	});
+        mRefresh.setOnClickListener(new OnClickListener(){
+        	public void onClick(View arg0) {
+        		
+        		
+        		
+        	};
+       	});
+        mMap.setOnClickListener(new OnClickListener(){
+        	public void onClick(View arg0) {
+        		Log.d("Debug","In mMap");
+        		Intent intent = new Intent();
+		    	intent.setClass(ShopList.this,Map.class);
+		    	startActivity(intent);
+        	};
+       	});
         mWifi.setOnClickListener(new OnClickListener(){
         	public void onClick(View arg0) {
         		Toast.makeText(getApplicationContext(), "Please activate WIFI and press Back to return to the application!", Toast.LENGTH_LONG).show();
@@ -167,14 +141,14 @@ public class ShopList extends Activity implements CreateNdefMessageCallback,
         		
         	};
        	});
-        
         // Register callback to set NDEF message
         mNfcAdapter.setNdefPushMessageCallback(this, this);
         // Register callback to listen for message-sent success
         mNfcAdapter.setOnNdefPushCompleteCallback(this, this);
     }
 
-    boolean checkWifiStatus()    {
+    
+	boolean checkWifiStatus()    {
     	ConnectivityManager connMgr = (ConnectivityManager)
     	this.getSystemService(Context.CONNECTIVITY_SERVICE);
     	android.net.NetworkInfo wifi = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
@@ -259,9 +233,140 @@ public class ShopList extends Activity implements CreateNdefMessageCallback,
             processIntent(getIntent());
             Log.d("Debug","Beam onResume1");
         }
+        
+        
+        //below is location operations
+        LocationManager status = (LocationManager)(this.getSystemService(Context.LOCATION_SERVICE));
+		
+		if(status.isProviderEnabled(LocationManager.GPS_PROVIDER)||status.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+			mgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+			Criteria criteria = new Criteria();
+		    best = mgr.getBestProvider(criteria, true);
+//		    mgr.requestLocationUpdates("gps", 60000, 1, this);
+//		    Location location = mgr.getLastKnownLocation("gps");
+		    mgr.requestLocationUpdates(best, 60000, 1, this);
+		    location = mgr.getLastKnownLocation(best);
+	        
+		} else {
+			new AlertDialog.Builder(ShopList.this)
+	       	.setTitle("No Location Service")
+	       	.setMessage("Please enable location service before use this app")
+	       	.setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+			   	public void onClick(DialogInterface dialog, int whichButton) {  
+			   	    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+			   	}
+	       	})
+	       	.show();
+		}
+		updateDistanceInfo();
     }
     
-    @Override
+    private void updateDistanceInfo(){
+    	Location shop = new Location("reverseGeocoded");
+        int Dist;
+        mList.clear();
+    	HashMap<String,Object> item = new HashMap<String,Object>();
+    	item.put("icon", R.drawable.hand);
+        item.put("shopname", "店名:歇手停");
+        shop.setLatitude(shopPose[0][0]);
+		shop.setLongitude(shopPose[0][1]);
+		Dist = (int)location.distanceTo(shop);
+		item.put("distance", (location == null ?"距離:未知":"距離:約"+getDistString(Dist) +"公里"));
+        mList.add(item);
+        
+        item = new HashMap<String,Object>();
+        item.put("icon", R.drawable.land);
+        item.put("shopname", "店名:69嵐");
+        shop.setLatitude(shopPose[1][0]);
+		shop.setLongitude(shopPose[1][1]);
+		Dist = (int)location.distanceTo(shop);
+        item.put("distance", (location == null ?"距離:未知":"距離:約"+getDistString(Dist)+"公里"));
+        mList.add(item);
+        
+        item = new HashMap<String,Object>();
+        item.put("icon", R.drawable.apple);
+        item.put("shopname", "店名:小蘋果");
+        shop.setLatitude(shopPose[2][0]);
+		shop.setLongitude(shopPose[2][1]);
+		Dist = (int)location.distanceTo(shop);
+        item.put("distance", (location == null ?"距離:未知":"距離:約"+getDistString(Dist) +"公里"));
+        mList.add(item);
+        
+        item = new HashMap<String,Object>();
+        item.put("icon", R.drawable.coffee);
+        item.put("shopname", "店名:老咖啡");
+        shop.setLatitude(shopPose[3][0]);
+		shop.setLongitude(shopPose[3][1]);
+		Dist = (int)location.distanceTo(shop);
+        item.put("distance", (location == null ?"距離:未知":"距離:約"+getDistString(Dist) +"公里"));
+        mList.add(item);
+        
+        item = new HashMap<String,Object>();
+        item.put("icon", R.drawable.family);
+        item.put("shopname", "店名:親家便利商店");
+        shop.setLatitude(shopPose[4][0]);
+		shop.setLongitude(shopPose[4][1]);
+		Dist = (int)location.distanceTo(shop);
+        item.put("distance", (location == null ?"距離:未知":"距離:約"+getDistString(Dist) +"公里"));
+        mList.add(item);
+        
+        item = new HashMap<String,Object>();
+        item.put("icon", R.drawable.fire);
+        item.put("shopname", "店名:阿火茶鋪");
+        shop.setLatitude(shopPose[5][0]);
+		shop.setLongitude(shopPose[5][1]);
+		Dist = (int)location.distanceTo(shop);
+        item.put("distance", (location == null ?"距離:未知":"距離:約"+getDistString(Dist) +"公里"));
+        mList.add(item);
+        
+        item = new HashMap<String,Object>();
+        item.put("icon", R.drawable.kaka);
+        item.put("shopname", "店名:KaKa真假");
+        shop.setLatitude(shopPose[6][0]);
+		shop.setLongitude(shopPose[6][1]);
+		Dist = (int)location.distanceTo(shop);
+        item.put("distance", (location == null ?"距離:未知":"距離:約"+getDistString(Dist)+"公里"));
+        mList.add(item);
+        
+        item = new HashMap<String,Object>();
+        item.put("icon", R.drawable.ko);
+        item.put("shopname", "店名:KO便利商店");
+        shop.setLatitude(shopPose[7][0]);
+		shop.setLongitude(shopPose[7][1]);
+		Dist = (int)location.distanceTo(shop);
+        item.put("distance", (location == null ?"距離:未知":"距離:約"+getDistString(Dist)+"公里"));
+        mList.add(item);
+        
+        item = new HashMap<String,Object>();
+        item.put("icon", R.drawable.one);
+        item.put("shopname", "ONE便利商店");
+        shop.setLatitude(shopPose[8][0]);
+		shop.setLongitude(shopPose[8][1]);
+		Dist = (int)location.distanceTo(shop);
+        item.put("distance", (location == null ?"距離:未知":"距離:約"+getDistString(Dist) +"公里"));
+        mList.add(item);
+        
+       	SimpleAdapter sAdapter;
+        sAdapter = new SimpleAdapter(this, mList, R.layout.listitem, 
+        		   new String[] {"icon","shopname","distance"}, 
+        		   new int[] {R.id.ItemImage,R.id.ItemTitle, R.id.ItemDist}  );
+
+        mListView.setAdapter(sAdapter);
+        mListView.setTextFilterEnabled(true);
+    	
+    	
+    }
+    
+    private String getDistString(int dist) {
+    	double x = dist*0.001;
+    	BigDecimal a = new BigDecimal(""+x);
+    	BigDecimal result = a.setScale(2, RoundingMode.DOWN);
+    	
+		return ""+result;
+	}
+
+
+	@Override
     public void onNewIntent(Intent intent) {
         // onResume gets called after this to handle the intent
     	Log.d("Debug","Beam onNewIntent");
@@ -310,6 +415,26 @@ public class ShopList extends Activity implements CreateNdefMessageCallback,
                 NdefRecord.TNF_MIME_MEDIA, mimeBytes, new byte[0], payload);
         return mimeRecord;
     }
+
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		//updateDistanceInfo();
+	}
+
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
 
     
     /*@Override
